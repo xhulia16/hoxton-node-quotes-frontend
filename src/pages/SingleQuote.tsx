bcb8b7a27112159f58af35c964b4c8f21716b20d
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-01 16:02:10 +0200

    patch method

--- src/pages/SingleQuote.tsx
+++ src/pages/SingleQuote.tsx
@@ -22,6 +22,7 @@ export function SingleQuote() {
 
   if (singleQuote === null) return <h1>Loading...</h1>;
   return (
+    <div>
     <div className="singleQuote">
       <h1>"{singleQuote.quote}"</h1>
       <img src={singleQuote.image}></img>
@@ -39,6 +40,39 @@ export function SingleQuote() {
       <Link to={"/quotes"}>
         <h4>Home</h4>
       </Link>
+    </div>
+    <div className="update-form">
+    <form
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          fetch(`http://localhost:5000/quotes/${params.itemId}`,{
+            method: "PATCH", 
+            headers: {
+              "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify({
+            quote: event.target.quote.value,
+            firstName: event.target.firstName.value,
+            lastName: event.target.lastName.value,
+            age: Number(event.target.age.value),
+            image: event.target.image.value,
+            })
+          })
+          .then(resp=>resp.json())
+          .then(data=> setSingleQuote(data))
+
+          event.target.reset();
+        }}
+      >
+        <input name="quote" placeholder="edit quote here..."></input>
+        <input name="firstName" placeholder="edit first name..."></input>
+        <input name="lastName" placeholder="edit last name..."></input>
+        <input name="age" placeholder="edit age..."></input>
+        <input name="image" placeholder="edit image address..."></input>
+        <button> Submit</button>
+      </form>
+    </div>
     </div>
   );
 }
