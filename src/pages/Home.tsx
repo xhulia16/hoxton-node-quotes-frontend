e73e11d08bd68e92bdc236030d2aa761b26f0309
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-02 00:48:22 +0200

    fixed post method for new quote

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Quote, Author } from "../types";
 
 export function Home() {
   const [quotes, setQuotes] = useState<Quote[]>([]);
   const [authors, setAuthors] = useState<Author[]>([]);
+
+let navigate=useNavigate()
 
   useEffect(() => {
     fetch("http://localhost:5000/quotes")
@@ -33,51 +35,78 @@ export function Home() {
           ))}
         </ul>
       </div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+      <div>
+        <form
+          className="quote-form"
+          onSubmit={(event) => {
+            event.preventDefault();
 
-          let firstName = event.target.firstName.value;
-          let lastName = event.target.lastName.value;
-          let authorOfQuote = authors.find(
-            (author) =>
-              author.firstName.toLowerCase() === firstName.toLowerCase() &&
-              author.lastName.toLowerCase() === lastName.toLowerCase()
-          );
-          console.log(firstName, lastName, authorOfQuote.id)
-          if(authorOfQuote && firstName && lastName){
-            fetch("http://localhost:5000/quotes",{
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json"
-                      },
-                      body: JSON.stringify({
-                      quote: event.target.quote.value,
-                      authorId: authorOfQuote.id
-                      })
-                    })
-                    .then(resp=>resp.json())
-                    .then(data=> setQuotes([...quotes, data]))
-          }
-
-                    // event.target.reset();
-        }}
-      >
-        <Link to={"/random-quote"}>
-          <h4>Click for random quote</h4>
-        </Link>
-        <h1>Post a quote here</h1>
-        <input name="quote" placeholder="insert quote here..."></input>
-        <input
-          name="firstName"
-          placeholder="insert first name of author..."
-        ></input>
-        <input
-          name="lastName"
-          placeholder="insert last name of author..."
-        ></input>
-        <button> Submit</button>
-      </form>
+            let authorFirstName = event.target.firstName.value;
+            let authorLastName = event.target.lastName.value;
+            let authorOfQuote = authors.find(
+              (author) =>
+                author.firstName.toLowerCase() ===
+                  authorFirstName.toLowerCase() &&
+                author.lastName.toLowerCase() === authorLastName.toLowerCase()
+            );
+            console.log(authorFirstName, authorLastName, authorOfQuote.id);
+            if (authorOfQuote && authorFirstName && authorLastName) {
+              fetch("http://localhost:5000/quotes", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  quote: event.target.quote.value,
+                  authorId: authorOfQuote.id,
+                }),
+              })
+              .then((resp) => resp.json())
+              .then(data=>navigate(`/quotes/${data.id}`))
+            }
+          }}
+        >
+          <Link to={"/random-quote"}>
+            <h4>Click for random quote</h4>
+          </Link>
+          <h1>Add a quote here</h1>
+          <input name="quote" placeholder="insert quote here..."></input>
+          <input
+            name="firstName"
+            placeholder="insert first name of author..."
+          ></input>
+          <input
+            name="lastName"
+            placeholder="insert last name of author..."
+          ></input>
+          <button> Submit</button>
+        </form>
+        <h1>Add an author</h1>
+        <form
+          className="author-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            fetch("http://localhost:5000/authors", {
+              method: "Post",
+              headers: {
+                "Content-Type": "json/application",
+              },
+              body: JSON.stringify({
+                firstName: event.target.firstName.value,
+                lastName: event.target.lastName.value,
+                age: event.target.age.value,
+                image: event.target.image.value,
+              }),
+            }).then((resp) => resp.json());
+          }}
+        >
+          <input name="firstName" placeholder="name of author"></input>
+          <input name="lastName" placeholder="Last name of author"></input>
+          <input name="age" placeholder="age of author"></input>
+          <input name="image" placeholder="image of author"></input>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
